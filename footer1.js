2562279d1cab7285e5106db2cb8365bda4824bc3
adi-paul-22 <ap4671@srmist.edu.in>
2023-03-02 22:53:39 +0530

    changes made

--- footer1.js
+++ footer1.js
@@ -18,7 +18,7 @@ const footerTemplate = `
             </ul>
           </div>
           <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-            <h5 class="text-uppercase">Men</h5>
+            <a href=men.html ><h5 class="text-uppercase">Men</h5></a>
             <ul class="list-unstyled mb-0">
               <li>
                 <a href="men.html" class="text-white">Shirts</a>
